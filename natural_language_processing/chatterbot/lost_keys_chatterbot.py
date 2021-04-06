b7d478c8ac6069d8528d01cc406c013370ef13ce
Kerollos Halim <halimkerollos@gmail.com>
2021-04-06 13:57:28 -1000

    Reformatted py file according to pep8.

--- natural_language_processing/chatterbot/lost_keys_chatterbot.py
+++ natural_language_processing/chatterbot/lost_keys_chatterbot.py
@@ -11,26 +11,29 @@ from chatterbot.trainers import ListTrainer
 # In[24]:
 
 
-bot = ChatBot('Lex')
+bot = ChatBot("Lex")
 
 
 # In[25]:
 
 
-bot = ChatBot('Lex',
-             storage_adapter='chatterbot.storage.SQLStorageAdapter',
-             database_url='sqlite:///database.sqlite3'
-             )
+bot = ChatBot(
+    "Lex",
+    storage_adapter="chatterbot.storage.SQLStorageAdapter",
+    database_url="sqlite:///database.sqlite3",
+)
 
 
 # In[26]:
 
 
-bot = ChatBot('Lex',
-             logic_adapters=[
-                 'chatterbot.logic.BestMatch',
-                 'chatterbot.logic.TimeLogicAdapter'
-             ])
+bot = ChatBot(
+    "Lex",
+    logic_adapters=[
+        "chatterbot.logic.BestMatch",
+        "chatterbot.logic.TimeLogicAdapter",
+    ],
+)
 
 
 # In[27]:
@@ -42,25 +45,27 @@ trainer = ListTrainer(bot)
 # In[28]:
 
 
-trainer.train([
-    'Hi',
-    'Hello',
-    'Did you check your pockets?',
-    'I lost my keys too',
-    'Where is the last place you remember seeing them?',
-    "I can't find my keys either...",
-    "That's unfortunate, where do you think they could've gone?",
-    'What exactly can I do about that?',
-    "Try looking between the couch cusions.",
-    "I guess they're gone then",
-    "Thanks! You really made my day!"
-])
+trainer.train(
+    [
+        "Hi",
+        "Hello",
+        "Did you check your pockets?",
+        "I lost my keys too",
+        "Where is the last place you remember seeing them?",
+        "I can't find my keys either...",
+        "That's unfortunate, where do you think they could've gone?",
+        "What exactly can I do about that?",
+        "Try looking between the couch cusions.",
+        "I guess they're gone then",
+        "Thanks! You really made my day!",
+    ]
+)
 
 
 # In[33]:
 
 
-response = bot.get_response('I lost my keys')
+response = bot.get_response("I lost my keys")
 
 
 # In[34]:
@@ -72,20 +77,20 @@ print("Bot response: ", response)
 # In[ ]:
 
 
-name=input('Enter your name')
-print("Welcome to the Unhelpful Bot Service {}!, Let me know what I can do for you...so I can ignore it.".format(name))
+name = input("Enter your name")
+print(
+    "Welcome to the Unhelpful Bot Service {}!, Let me know what I can do for you...so I can ignore it.".format(
+        name
+    )
+)
 while True:
-    request=input(name+':')
-    if request=='Bye' or request=='bye':
-        print('Bot: Bye')
+    request = input(name + ":")
+    if request == "Bye" or request == "bye":
+        print("Bot: Bye")
         break
     else:
-        response=bot.get_response(request)
-        print('Bot: ', response)
+        response = bot.get_response(request)
+        print("Bot: ", response)
 
 
 # In[ ]:
-
-
-
-
